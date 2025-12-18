7b0f0df267220f9e7147157aa97bd5d78d8d352f
amarpatil8397-dev <amarpatil8397@gmail.com>
2025-12-18 22:13:21 +0530

    assignment6

---
+++ day06/moiscreate.py
@@ -0,0 +1,38 @@
+# import mysql connector
+import mysql.connector
+
+# establish connection with mysql server
+connection = mysql.connector.connect(
+    host = "127.0.0.1",
+    port = 3306,
+    user = "root",
+    password = "root",
+    database = "smart_agriculture",
+    use_pure = True
+)
+
+# form a query to be executed
+id = int(input("Enter id : "))
+moisture = input("Enter moisture : ")
+#date = input("Enter humidity : ")
+
+#timestamp = input("Enter timestamp : ")
+from datetime import datetime
+date_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+query = f"insert into soil_moisture values({id}, '{moisture}',  '{date_time}');"
+
+# create a cursor to execute a query
+cursor = connection.cursor()
+
+# execute a query
+cursor.execute(query)
+
+# commit your changes on mysql server
+connection.commit()
+
+# close the cursor
+cursor.close()
+
+# close the connection with mysql server
+connection.close()
